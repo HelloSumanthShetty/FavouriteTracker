fff9b9a179b06f2df1328af948c759da7af1a770
sumanth u shetty <sumanthshetty883@gmail.com>
2025-11-01 06:43:38 +0530

    production

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,9 +39,9 @@ function App() {
           </div>
           <div className=" z-10 w-full">
             <Routes>
-              <Route path="/auth" element={token ? <Navigate to="/home" /> : <Login />} />
-              <Route path="/home" element={token ? <HeroSection theme={theme} setTheme={settheme} /> : <Navigate to="/auth" />} />
-                <Route path="*" element={<Navigate to={token! ?  "/auth":"/home"} />} />
+              <Route path="/auth" element={token ? <Navigate to="/" /> : <Login />} />
+              <Route path="/" element={token ? <HeroSection theme={theme} setTheme={settheme} /> : <Navigate to="/auth" />} />
+                <Route path="*" element={<Navigate to={token! ?  "/auth":"/"} />} />
             </Routes>
           </div>
         </div>
